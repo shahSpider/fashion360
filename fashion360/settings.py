da431f2d9b5c088dc44f45f6a1edd3f2213bc9d6
shahspider@github.com <aq.syed007@gmail.com>
2024-08-24 13:00:36 +0500

    feat: config database and initiate a CRM app

--- fashion360/settings.py
+++ fashion360/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'fashion360.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fashion360_db',
+        'USER': 'fashion360',
+        'PASSWORD': 'fashion360',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
